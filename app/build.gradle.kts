dd6d0cef5b6965b41935657724c3f03afcd67a06
Ankit Kumar <ankit.steven007@gmail.com>
2025-09-09 03:00:10 +0530

    Update plugin ID and change license to Apache 2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
-    id("com.sparrow.skyhigh.16kb-doctor")
+    id("io.github.sparrow007.skyhigh.16kb-doctor")
 }
 
 
--- skyhigh-16kb-doctor/plugin/build.gradle.kts
+++ skyhigh-16kb-doctor/plugin/build.gradle.kts
@@ -1,28 +1,30 @@
 import java.util.Base64
 
 
-val ossrhUsernameProp = providers.gradleProperty("OSSRHUsername")
-
-
-val ossrhPasswordProp = providers.gradleProperty("OSSRHPassword")
-
-
-
-logger.lifecycle("OSSRH Username provided: ${ossrhUsernameProp.get()}")
-logger.lifecycle("OSSRH Password provided: ${!ossrhPasswordProp.orNull.isNullOrBlank()}")
+//val ossrhUsernameProp = providers.gradleProperty("OSSRHUsername")
+//
+//
+//val ossrhPasswordProp = providers.gradleProperty("OSSRHPassword")
+//
+//
+//
+//logger.lifecycle("OSSRH Username provided: ${ossrhUsernameProp.get()}")
+//logger.lifecycle("OSSRH Password provided: ${!ossrhPasswordProp.orNull.isNullOrBlank()}")
 
 
 plugins {
     // Apply the Java Gradle plugin development plugin to add support for developing Gradle plugins
     `java-gradle-plugin`
     `kotlin-dsl`
-    `maven-publish`
-    `signing`
+    signing
     id("com.gradle.plugin-publish") version "1.2.1"
     kotlin("jvm") version "1.9.20"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20"
     id("com.vanniktech.maven.publish") version "0.34.0"
 }
+
+
+
 
 repositories {
     // Use Maven Central for resolving dependencies.
@@ -57,41 +59,13 @@ sourceSets {
 }
 
 
-testing {
-    suites {
-        // Configure the built-in test suite
-        val test by getting(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
-            useKotlinTest("2.1.0")
-        }
-
-        // Create a new test suite
-        val functionalTest by registering(JvmTestSuite::class) {
-            // Use Kotlin Test test framework
-            useKotlinTest("2.1.0")
-
-            dependencies {
-                // functionalTest test suite depends on the production code in tests
-                implementation(project())
-            }
-
-            targets {
-                all {
-                    // This test suite should run after the built-in test suite has run its tests
-                    testTask.configure { shouldRunAfter(test) }
-                }
-            }
-        }
-    }
-}
-
 gradlePlugin {
     website.set("https://github.com/sparrow007/skyhigh-16kb-doctor")
     vcsUrl.set("https://github.com/sparrow007/skyhigh-16kb-doctor")
 
     plugins {
         create("skyhighDoctorPlugin") {
-            id = "com.sparrow.skyhigh.16kb-doctor"
+            id = "io.github.sparrow007.skyhigh.16kb-doctor"
             implementationClass = "com.sparrow.plugin.SkyHighDoctorPlugin"
             displayName = "SkyHigh 16KB Page Alignment Doctor"
             description = "Gradle plugin to scan Android apps for 16KB page alignment compatibility"
@@ -100,89 +74,120 @@ gradlePlugin {
     }
 }
 
-gradlePlugin.testSourceSets.add(sourceSets["functionalTest"])
-
-tasks.named<Task>("check") {
-    // Include functionalTest as part of the check lifecycle
-    dependsOn(testing.suites.named("functionalTest"))
-}
 
 tasks.test {
     useJUnitPlatform()
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["java"])
-            groupId = "io.github.sparrow007"
-            artifactId = "skyhigh-16kb-doctor"
-            version = "1.0.0"
-
-            pom {
-                name.set("SkyHigh 16KB Page Alignment Doctor")
-                description.set("Gradle plugin to scan Android apps for 16KB page alignment compatibility")
-                url.set("https://github.com/sparrow007/skyhigh-16kb-doctor")
-
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-
-                developers {
-                    developer {
-                        id.set("sparrow007")
-                        email.set("ankit.steven007@gmail.com")
-                    }
-                }
-
-                scm {
-                    connection.set("scm:git:git://github.com/sparrow007/skyhigh-16kb-doctor.git")
-                    developerConnection.set("scm:git:ssh://github.com:sparrow007/skyhigh-16kb-doctor.git")
-                    url.set("https://github.com/sparrow007/skyhigh-16kb-doctor")
-                }
-            }
-        }
-
-        repositories {
-            maven {
-                name = "OSSRH"
-                url = uri("https://central.sonatype.com/api/v1/publisher/upload")
-                credentials {
-                    username = ossrhUsernameProp.get()
-                    password = ossrhPasswordProp.get()
-                }
-            }
-        }
-    }
-}
-
-
-signing {
-    val key = providers.gradleProperty("OSSRHKeyAnkit")
-    val password = providers.gradleProperty("OSSRHPasswordAA")
-    logger.lifecycle("Signing key provided: ${!password.get().isNullOrBlank()}, password provided: ${password.get()}")
-    logger.lifecycle("Signing key provided: ${!key.get().isNullOrBlank()}, password provided: ${key.get()}")
-    //print the aboves
-
-
-    if (key.get().isNotBlank() && password.get().isNotBlank()) {
-        val decodedKey = String(Base64.getDecoder().decode(key.get()))
-        useInMemoryPgpKeys(decodedKey, password.get())
-        sign(publishing.publications)
-        println("✅ Signing enabled for publications")
-    } else {
-        println("⚠️ Signing disabled: missing signing.key or signing.password")
-    }
-}
+//publishing {
+//    publications {
+//        create<MavenPublication>("maven") {
+//            from(components["java"])
+//            groupId = "io.github.sparrow007"
+//            artifactId = "skyhigh-16kb-doctor"
+//            version = "1.0.0-SNAPSHOT"
+//
+//            pom {
+//                name.set("SkyHigh 16KB Page Alignment Doctor")
+//                description.set("Gradle plugin to scan Android apps for 16KB page alignment compatibility")
+//                url.set("https://github.com/sparrow007/skyhigh-16kb-doctor")
+//
+//                licenses {
+//                    license {
+//                        name.set("The Apache License, Version 2.0")
+//                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+//                    }
+//                }
+//
+//                developers {
+//                    developer {
+//                        id.set("sparrow007")
+//                        email.set("ankit.steven007@gmail.com")
+//                    }
+//                }
+//
+//                scm {
+//                    connection.set("scm:git:git://github.com/sparrow007/skyhigh-16kb-doctor.git")
+//                    developerConnection.set("scm:git:ssh://github.com:sparrow007/skyhigh-16kb-doctor.git")
+//                    url.set("https://github.com/sparrow007/skyhigh-16kb-doctor")
+//                }
+//            }
+//        }
+//
+//        repositories {
+//            maven {
+//                name = "OSSRH"
+//                url = uri("https://central.sonatype.com/api/v1/publisher/upload")
+//                credentials {
+//                    username = ossrhUsernameProp.get()
+//                    password = ossrhPasswordProp.get()
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//
+//signing {
+//    val key = providers.gradleProperty("OSSRHKeyAnkit")
+//    val password = providers.gradleProperty("OSSRHPasswordAA")
+//    logger.lifecycle("Signing key provided: ${!password.get().isNullOrBlank()}, password provided: ${password.get()}")
+//    logger.lifecycle("Signing key provided: ${!key.get().isNullOrBlank()}, password provided: ${key.get()}")
+//    //print the aboves
+//
+//
+//    if (key.get().isNotBlank() && password.get().isNotBlank()) {
+//        val decodedKey = String(Base64.getDecoder().decode(key.get()))
+//        useInMemoryPgpKeys(decodedKey, password.get())
+//        sign(publishing.publications)
+//        println("✅ Signing enabled for publications")
+//    } else {
+//        println("⚠️ Signing disabled: missing signing.key or signing.password")
+//    }
+//}
 
 tasks.withType<PublishToMavenRepository>().configureEach {
     dependsOn(tasks.withType<Sign>())
 }
 
-tasks.named("publishMavenPublicationToMavenLocal") {
-    dependsOn(tasks.named("signPluginMavenPublication"))
+//tasks.named("publishMavenPublicationToMavenLocal") {
+//    dependsOn(tasks.named("signPluginMavenPublication"))
+//}
+// Maven Publish from another Library
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
 }
+
+mavenPublishing {
+    coordinates("io.github.sparrow007", "skyhigh-16kb-doctor", "1.0.0")
+
+    pom {
+        name.set("SkyHigh 16KB Doctor")
+        description.set("A Gradle plugin to scan Android apps for 16KB page alignment compatibility")
+        inceptionYear.set("2025")
+        url.set("https://github.com/sparrow007/skyhigh-16kb-doctor")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("sparrow007")
+                email.set("ankit.steven007@gmail.com")
+                name.set("Ankit")
+                url.set("https://github.com/sparrow007/")
+            }
+        }
+        scm {
+            connection.set("scm:git:git://github.com/sparrow007/skyhigh-16kb-doctor.git")
+            developerConnection.set("scm:git:ssh://github.com:sparrow007/skyhigh-16kb-doctor.git")
+            url.set("https://github.com/sparrow007/skyhigh-16kb-doctor")
+        }
+    }
+}
+
 

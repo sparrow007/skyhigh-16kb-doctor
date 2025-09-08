e10d7b3aef0484d0a70af47c52a19830b3804caf
Ankit Kumar <ankit.steven007@gmail.com>
2025-09-09 03:24:55 +0530

    Refactor SkyHigh Doctor plugin tasks and update version to 1.0.1

--- skyhigh-16kb-doctor/plugin/build.gradle.kts
+++ skyhigh-16kb-doctor/plugin/build.gradle.kts
@@ -1,17 +1,3 @@
-import java.util.Base64
-
-
-//val ossrhUsernameProp = providers.gradleProperty("OSSRHUsername")
-//
-//
-//val ossrhPasswordProp = providers.gradleProperty("OSSRHPassword")
-//
-//
-//
-//logger.lifecycle("OSSRH Username provided: ${ossrhUsernameProp.get()}")
-//logger.lifecycle("OSSRH Password provided: ${!ossrhPasswordProp.orNull.isNullOrBlank()}")
-
-
 plugins {
     // Apply the Java Gradle plugin development plugin to add support for developing Gradle plugins
     `java-gradle-plugin`
@@ -24,10 +10,7 @@ plugins {
 }
 
 
-
-
 repositories {
-    // Use Maven Central for resolving dependencies.
     mavenCentral()
 }
 
@@ -79,88 +62,13 @@ tasks.test {
     useJUnitPlatform()
 }
 
-//publishing {
-//    publications {
-//        create<MavenPublication>("maven") {
-//            from(components["java"])
-//            groupId = "io.github.sparrow007"
-//            artifactId = "skyhigh-16kb-doctor"
-//            version = "1.0.0-SNAPSHOT"
-//
-//            pom {
-//                name.set("SkyHigh 16KB Page Alignment Doctor")
-//                description.set("Gradle plugin to scan Android apps for 16KB page alignment compatibility")
-//                url.set("https://github.com/sparrow007/skyhigh-16kb-doctor")
-//
-//                licenses {
-//                    license {
-//                        name.set("The Apache License, Version 2.0")
-//                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-//                    }
-//                }
-//
-//                developers {
-//                    developer {
-//                        id.set("sparrow007")
-//                        email.set("ankit.steven007@gmail.com")
-//                    }
-//                }
-//
-//                scm {
-//                    connection.set("scm:git:git://github.com/sparrow007/skyhigh-16kb-doctor.git")
-//                    developerConnection.set("scm:git:ssh://github.com:sparrow007/skyhigh-16kb-doctor.git")
-//                    url.set("https://github.com/sparrow007/skyhigh-16kb-doctor")
-//                }
-//            }
-//        }
-//
-//        repositories {
-//            maven {
-//                name = "OSSRH"
-//                url = uri("https://central.sonatype.com/api/v1/publisher/upload")
-//                credentials {
-//                    username = ossrhUsernameProp.get()
-//                    password = ossrhPasswordProp.get()
-//                }
-//            }
-//        }
-//    }
-//}
-//
-//
-//signing {
-//    val key = providers.gradleProperty("OSSRHKeyAnkit")
-//    val password = providers.gradleProperty("OSSRHPasswordAA")
-//    logger.lifecycle("Signing key provided: ${!password.get().isNullOrBlank()}, password provided: ${password.get()}")
-//    logger.lifecycle("Signing key provided: ${!key.get().isNullOrBlank()}, password provided: ${key.get()}")
-//    //print the aboves
-//
-//
-//    if (key.get().isNotBlank() && password.get().isNotBlank()) {
-//        val decodedKey = String(Base64.getDecoder().decode(key.get()))
-//        useInMemoryPgpKeys(decodedKey, password.get())
-//        sign(publishing.publications)
-//        println("✅ Signing enabled for publications")
-//    } else {
-//        println("⚠️ Signing disabled: missing signing.key or signing.password")
-//    }
-//}
-
-tasks.withType<PublishToMavenRepository>().configureEach {
-    dependsOn(tasks.withType<Sign>())
-}
-
-//tasks.named("publishMavenPublicationToMavenLocal") {
-//    dependsOn(tasks.named("signPluginMavenPublication"))
-//}
-// Maven Publish from another Library
 mavenPublishing {
     publishToMavenCentral()
     signAllPublications()
 }
 
 mavenPublishing {
-    coordinates("io.github.sparrow007", "skyhigh-16kb-doctor", "1.0.0")
+    coordinates("io.github.sparrow007", "skyhigh-16kb-doctor", "1.0.1")
 
     pom {
         name.set("SkyHigh 16KB Doctor")
